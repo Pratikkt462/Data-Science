a58749b2f5a0f7c0ade548b621b21aeb0b087459
Pratikkt462 <159098494+Pratikkt462@users.noreply.github.com>
2024-04-25 20:11:33 +0530

    Add files via upload

---
+++ 10-Association_Rule/Groceries_assignment.py
@@ -0,0 +1,29 @@
+
+
+'''*****************************Groceries.csv***********************'''
+'''
+Business Objective:
+    Minimize: To increase the sell of daily use product.
+
+    Maximize: 
+    
+    Business Constraints:
+        Changing demand of customer as per trends of market.
+'''
+
+'''****************************Data Dictionary ************************
+Name of Feature            Description               Type                Relevance
+
+
+
+
+
+
+
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+grocery=pd.read_csv('c:/2-datasets/groceries.csv')
+grocery.columns
+grocery.dtypes
